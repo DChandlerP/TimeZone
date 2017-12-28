c1d105b2f33619bcfa9cd36c49f7cf128641b1f5
p2758145 <c-david.prince1@charter.com>
2017-12-28 12:15:09 -0600

    Zoned datetime example

--- src/TimeZone.java
+++ src/TimeZone.java
@@ -2,10 +2,11 @@ import java.time.*;
 
 public class TimeZone {
     public static void main(String[] args) {
-        Instant local = Instant.now().minus(Duration.ofHours(6));
+        Instant minus = Instant.now().minus(Duration.ofHours(6));
         Instant utc = Instant.now();
+        ZonedDateTime zonedDateTime = utc.atZone(ZoneId.of("US/Mountain"));
         LocalDate localDate = LocalDate.now(ZoneId.of("US/Mountain"));
 
-        System.out.println(localDate);
+        System.out.println(zonedDateTime);
     }
 }
